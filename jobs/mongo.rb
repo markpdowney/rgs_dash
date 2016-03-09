6726f3856ebb41dded628f9a9fa4f1278a650820
markpdowney <markpdowney@gmail.com>
2016-03-09 16:42:22 +0000

    v0.1.4 Mongo

--- jobs/mongo.rb
+++ jobs/mongo.rb
@@ -5,7 +5,7 @@ include Mongo
 SCHEDULER.every '15m', :first_in => 0 do |job|
 	db = Mongo::Client.new(['127.0.0.1:27017'], :database => 'readgoodstuff')
 
-	send_event('mongo_post_count', client[:posts].count())
-	send_event('mongo_user_count', client[:users].count())
+	send_event('mongo_post_count', db[:posts].count())
+	send_event('mongo_user_count', db[:users].count())
 end
 
