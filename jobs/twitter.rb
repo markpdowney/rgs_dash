b3edd94c06224809bf04c0b8be24fa201b8fbfff
markpdowney <markpdowney@gmail.com>
2016-03-09 12:18:52 +0000

    v0.1.4 Twtr Followers

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,28 +1,21 @@
-require 'twitter'
+#!/usr/bin/env ruby
+require 'nokogiri'
+require 'open-uri'
 
+# Track public available information of a twitter user like follower, follower
+# and tweet count by scraping the user profile page.
 
-#### Get your twitter keys & secrets:
-#### https://dev.twitter.com/docs/auth/tokens-devtwittercom
-twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'mhTHO9DSJM9biUBHQDRxYBCIj'
-  config.consumer_secret = '  TCk3WuwFDxntPvwasivDWbX25LPLwimLFhY7zskZywykrPJV1X'
-  config.access_token = ' 4217786393-Uqr0wcJlOP2G6V7LWEuDzepyyef6h3ILBsGGsgc'
-  config.access_token_secret = 'Ts3HVeUfztYsKbjquyYpr7rRHJgXO7ndT6oGtukUvpXk8'
-end
+# Config
+# ------
+twitter_username = ENV['TWITTER_USERNAME'] || 'foobugs'
 
-search_term = URI::encode('@read_good_stuff')
+SCHEDULER.every '2m', :first_in => 0 do |job|
+  doc = Nokogiri::HTML(open("https://twitter.com/#{twitter_username}"))
+  tweets = doc.css('a[data-nav=tweets]').first.attributes['title'].value.split(' ').first
+  followers = doc.css('a[data-nav=followers]').first.attributes['title'].value.split(' ').first
+  following = doc.css('a[data-nav=following]').first.attributes['title'].value.split(' ').first
 
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  begin
-    tweets = twitter.search("#{search_term}")
-
-    if tweets
-      tweets = tweets.map do |tweet|
-        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
-      end
-      send_event('twitter_mentions', comments: tweets)
-    end
-  rescue Twitter::Error
-    puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
-  end
+  send_event('twitter_user_tweets', current: tweets)
+  send_event('twitter_user_followers', current: followers)
+  send_event('twitter_user_following', current: following)
 end
